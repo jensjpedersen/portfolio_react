6ed8c7b6ae8772d171287568af2d868c277c9860
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2024-01-03 16:42:50 +0100

    fix: indentation formating from TS

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,46 +8,51 @@ console.log(heroImage);
 // console.log(heroImage);
 //
 const StyledHero = styled.section`
-  div#image-container {
+div#image-container {
     position: relative;
 
     padding: ${variables.navHeight};
-    padding-top: 0;
+        padding-top: 0;
     width: 100vw;
     min-width: 600px; 
     min-height: 300px;
-  
+
     height: calc(100vh - ${variables.navHeight});
 
     img {
-      height: 100%;
-      width: 100%;
-      object-fit: cover;
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+        border-radius: 10px; 
+        // box-shadow: 0 0 20px 5px rgba(22, 49, 58, 1);
+        box-shadow: 0 0 5px 0px rgba(0, 0, 0, 1);
+        border-color: rgba(22, 49, 58, 1);
     }
 
     .hero-text {
-      position: absolute;
-      bottom: 40%;
-      left: 20%;
-      // top: 100%;
+        position: absolute;
+        bottom: 40%;
+        left: 20%;
+        // top: 100%;
 
     }
 
-  }
+}
 
-  @media ${variables.media.sm} {
+@media ${variables.media.sm} {
     div#image-container {
-      padding: 0;
-      min-width: 0;
+        padding: 0;
+        min-width: 0;
 
-      .hero-text {
-        bottom: 20%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    
-      }
+        .hero-text {
+            bottom: 20%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            min-width: 300px; 
+
+        }
     }
-  }
+}
 
 `
 
