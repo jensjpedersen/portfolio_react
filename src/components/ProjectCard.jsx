dd3c30a86a7a962c11236443ed0f66aaaa6f4f45
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2024-01-13 16:53:21 +0100

    feat: project card hover effect.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,28 +4,64 @@ import styled from 'styled-components'
 const StyledProjectCard = styled.div`
 
     background-color: gray; 
-    width: 500px;
+    width: 400px;
     height: 200px;
+    position: relative;
+    border-radius: 10px;
+
+    overflow: hidden;
 
 
+    .background-image {
+        width: 100%; 
+        height: 100%;
+        background-image: url(${props => props.img});
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+
+    }
+
+    .overlay {
+        position: absolute;
+        top: -50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        transition: top 0.5s ease-in-out;
+    }
+
+
+    &:hover .overlay {
+        width: 100%;
+        height: 100%;
+        top: 50%;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: flex; 
+        justify-content: center;
+        align-items: center;
+
+    }
+
+
+    
 `
 
-
-
 export default function ProjectCard({ img }) {
+    console.log('hello', img);
 
     return (
-        <StyledProjectCard>
-            <h3>Project title</h3>
+        <StyledProjectCard img={img}>
+            <div className="background-image"></div>
+            <div className="overlay">
 
-            <p>Proejct discriptio: Adipisicing ullam hic officia dicta mollitia. Earum aperiam natus perspiciatis animi fugit! Quia eveniet sint ipsa eos incidunt vero. Voluptatem quisquam dolore consequuntur alias tempora sequi Nam architecto voluptate odio</p>
+                <div className="text">
+                    <h3>Project title</h3>
+                    <p>Short description </p>
+                </div>
+            </div>
         </StyledProjectCard>
 
 
     )
-
-
-
-
 
 }
